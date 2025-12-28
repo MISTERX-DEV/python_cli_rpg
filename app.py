14d1179818692000ea9453586f68f2251da06571
MISTERX-DEV <mr.nikishin009@gmail.com>
2025-12-28 10:09:04 +0300

    Add test level up

--- app.py
+++ app.py
@@ -14,12 +14,11 @@ player_default = {
     "stamina":0,
     "level":1,
     "xp":0,
-    "charactetistics":{
+    "characteristics":{
         "health_c":20,
         "stamina_c":3,
         "strength_c":5,
         "dexterity_c":5
-        
     },
     "equipment":{
         "weapon":data.get_item_by_id(1),
@@ -60,13 +59,13 @@ monster = {
         "desc": data.get_monster_by_id(1)[2],
         "health": data.get_monster_by_id(1)[3],
         "attack_power": data.get_monster_by_id(1)[4],
-        "accurancy": data.get_monster_by_id(1)[5],
+        "accuracy": data.get_monster_by_id(1)[5],
         "xp": data.get_monster_by_id(1)[6]
     }
 
 
 def view_monstro(monster):
-    print(f''' [ {monster["name"]} ] \n [ HP:{monster["health"]} ] \n [ ATK:{monster["attack_power"]} ] \n [ ACR:{monster["accurancy"]} ]''')
+    print(f''' [ {monster["name"]} ] \n [ HP:{monster["health"]} ] \n [ ATK:{monster["attack_power"]} ] \n [ ACR:{monster["accuracy"]} ]''')
 def view_player_characteristics(player):
     print(f'\nХарактеристики игрока: \nЗдоровье: [ {player["health"]} ] \nВыносливость: [ {player["stamina"]} ] \nОружие: {player["equipment"]["weapon"][1]} | ATK: {player["equipment"]["weapon"][3]} | ACR: {player["equipment"]["weapon"][4]}')
 
@@ -94,7 +93,7 @@ def fight():
                 cls()
                 print("Вы промахнулись!")
         elif action == 2:
-            if monster_hit_chance <= monster["accurancy"]:
+            if monster_hit_chance <= monster["accuracy"]:
                 player["health"] -= monster["attack_power"]
                 cls()
                 print(f"Монстр нанес вам {monster['attack_power']} урона!")
@@ -114,11 +113,27 @@ def kill_monster():
     player["xp"] += monster["xp"]
 
 def level_up():
-    pass
+
+    while True:
+        action = int(input("\n[1] Повысить xp\n[2] Повысить уровень\n------> "))
+        if action == 1:
+            player["xp"] = int(input("Введите количество xp: "))
+        elif action == 2:
+            if player["xp"] >= 100:
+                player["level"] += 1
+                player["xp"] -= 100
+                player["characteristics"]["health_c"] += 1
+                player["characteristics"]["stamina_c"] += 1
+                player["characteristics"]["strength_c"] += 1
+                player["characteristics"]["dexterity_c"] += 1
+                print("Поздравляем! Вы повысили уровень!")
+                updating_the_characteristics()
+                view_player_characteristics(player)
+                break
 
 def updating_the_characteristics():
-    player["health"] = player["charactetistics"]["health_c"]
-    player["stamina"] = player["charactetistics"]["stamina_c"]
+    player["health"] = player["characteristics"]["health_c"]
+    player["stamina"] = player["characteristics"]["stamina_c"]
     data.create_player(main.player_name, player)
 
 def boss():
@@ -131,7 +146,7 @@ def boss():
 
 # Вызов
 #fight()
-#level_up()
+level_up()
 
 
 
